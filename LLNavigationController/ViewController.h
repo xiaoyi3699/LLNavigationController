6d289f6fe902936b5aa4295cc96168af1527bd80
wangzhaomeng <122589615@qq.com>
2016-12-22 13:31:46 +0800

    Initial Commit

---
+++ LLNavigationController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LLNavigationController
+//
+//  Created by Mr.Wang on 16/12/22.
+//  Copyright © 2016年 MaoChao Network Co. Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LLNavigationController/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LLNavigationController
+//
+//  Created by Mr.Wang on 16/12/22.
+//  Copyright © 2016年 MaoChao Network Co. Ltd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
